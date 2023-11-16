d6d4463e484679d3697a69af655139e9ca173989
Alessandro-Ciaboco <51087879+Alessandro-Ciaboco@users.noreply.github.com>
2023-11-16 20:20:24 +0100

    Update data_management.py
    now we can load the highest score of the session in a txt and pull out all the scores. we could implement easy a ranking

--- src/backend/data_management.py
+++ src/backend/data_management.py
@@ -1,6 +1,12 @@
 class DataManagement:
     def save_high_score(self, score):
+        with open('high_scores.txt', 'w') as file:
+            file.write(f"{score}\n")
         pass
 
     def load_high_score(self):
-        pass
+        scores = []
+        with open('high_scores.txt', 'r') as file:
+            lines = file.readlines()
+            scores = [int(score.strip()) for score in lines]
+        return scores
