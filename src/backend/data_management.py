c56521f26da707a0ec660d0967d12b5b6fd22a2f
Alessandro-Ciaboco <51087879+Alessandro-Ciaboco@users.noreply.github.com>
2023-11-18 11:29:40 +0100

    Update data_management.py
    save name and make ranking

--- src/backend/data_management.py
+++ src/backend/data_management.py
@@ -1,12 +1,14 @@
 class DataManagement:
-    def save_high_score(self, score):
-        with open('high_scores.txt', 'w') as file:
-            file.write(f"{score}\n")
-        pass
+    def save_high_score(self, name, score):
+        with open('high_scores.txt', 'a') as file:
+            file.write(f"{name}:{score}\n")
+
 
     def load_high_score(self):
         scores = []
         with open('high_scores.txt', 'r') as file:
             lines = file.readlines()
-            scores = [int(score.strip()) for score in lines]
-        return scores
+            scores = [line.strip().split(':') for line in lines]
+            sorted_scores = sorted(scores, key=lambda x: int(x[1]), reverse=True)
+            return sorted_scores
+
